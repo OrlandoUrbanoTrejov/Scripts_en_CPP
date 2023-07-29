0fb173e1431430a0a0bc597aac2483fef26d5706
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-28 19:06:53 -0600

    Convertir una cadena de texto a mayuscula

---
+++ Ejercicio51.cpp
@@ -0,0 +1,18 @@
+/* Autor:  Orlando Urbano Trejo (Lando)
+ * Fecha:  27-07-2023 
+ * Correo: urbanorlando79@gmail.com
+ *
+ * Ejercicio: Convierte a mayuscula cualquier cadena de texto */
+
+#include <iostream>
+#include <string.h>
+
+int main(){
+    char Palabra[20];
+	 std::cout << "Ingresa una palabra en mayuscula: ";
+    std::cin >> Palabra[20];
+	 std::cout << "Cadena en Mayuscula: " << Palabra << std::endl;
+	 std::cout << "Cadena en Minuscula: " << strlen(Palabra) << std::endl;  
+    return 0; 
+}
+  
